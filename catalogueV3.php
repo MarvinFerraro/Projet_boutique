edb7e224c2249407874267ed6ee43ea33d2d71f8
MarvinFerraro <marvin.ferraro@le-campus-numerique.fr>
2020-02-01 17:45:22 +0100

    ajout et modif du panier

--- catalogueV3.php
+++ catalogueV3.php
@@ -1,12 +1,34 @@
 <?php
 // Init array multy
-include("include/functions.php");
+require 'include/functions.php';
  include("include/head.php") ?>
+<!---->
+<!--<form action="panier.php" method="post">-->
+<!--    --><?php
+//    afficheArticle($cats);
+//    ?>
+<!--    <input type="submit" value="Envoyer">-->
+<!--</form>-->
+<?php
+    $cats = listCats();
+    $i=0;
 
-<form action="panier.php" method="post">
-    <?php
-    afficheArticle($cats);
     ?>
-    <input type="submit" value="envoersqdsq">
-</form>
+
+    <form action="panier.php" method="post">
+        <?php
+
+        foreach ($cats as $cat => $value) { ?>
+            <div class="cadre article">
+                <h2 class="nom">Adresse <?= $value[0]?></h2>
+                <img src='<?=$value[1]?>' alt="Photo de '<?=$value[0]?>'' ">
+                <p class="price">Pour seulement : <?=$value[2]?> € <span class="price_text">(Transport compris)</span></p>
+                <input type="checkbox" name="article[]" value ="<?=$i?>" id="">
+            </div>
+            <?php
+            $i++;
+        }
+        ?>
+        <input type="submit" value="Envoyer">
+    </form>
 <?php include("include/footer.php") ?>
--- panier.php
+++ panier.php
@@ -1,8 +1,6 @@
 <?php
-include ('include/functions.php');
+require 'include/functions.php';
 include ('include/head.php');
-
-
 //$panier =[];
 //$somme = 0;
 //foreach ($_POST['article'] as $key => $item){
@@ -12,18 +10,57 @@ include ('include/head.php');
 //}
 //afficheArticle($panier);
 //var_dump($panier);
-//?>
-<!--<p class="pricePanier"> La somme totale est de : --><?//= $somme?><!--</p>-->
-<!---->
-<?php
-
-
-
-
-foreach ($_POST['article'] as $key => $item){
-    $item = $cats[$key];
-    var_dump($item);
-    afficheArticle([$item]);
+//
+//foreach ($_POST['article'] as $key => $item){
+//    $item = $cats[$key];
+//    var_dump($item);
+//    afficheArticle([$item]);
+//}
+$cats = listCats();
+$catsNew = [];
+if (!empty($_POST['article'])) {
+    $selected_cats = $_POST['article'];
+    $somme = 0;
+    ?>
+    <form action="panier.php" method="post">
+        <?php
+        foreach ($selected_cats as $article) {
+            ?>
+            <div class="cadre article">
+                <h2 class="nom">Adresse <?= $cats[$article][0] ?></h2>
+                <img src='<?= $cats[$article][1] ?>' alt="Photo de '<?= $cats[$article][0] ?> ">
+                <p class="price">Pour seulement : <?= $cats[$article][2] ?> € <span class="price_text">(Transport compris)</span>
+                </p>
+                <input type="checkbox" name="removeArticle[]" value="<?= $article ?>" id="">
+            </div>
+            <?php
+            $somme += $cats[$article][2];
+        }
+        var_dump($catsNew);
+        ?>
+        <input type="submit" value="Modifié panier">
+    </form>
+    <p> La somme est de <?= $somme ?></p>
+    <?php
+} elseif (!empty($_POST['removeArticle'])) {
+    $removeCats = $_POST['removeArticle'];
+    foreach ($cats as $cat => $value) {
+        if (!in_array($cats, $removeCats)) {
+            ?>
+            <div class="cadre article">
+                <h2 class="nom">Adresse <?= $value[0] ?></h2>
+                <img src='<?= $value[1] ?>' alt="Photo de '<?= $value[0] ?>'' ">
+                <p class="price">Pour seulement : <?= $value[2] ?> € <span class="price_text">(Transport compris)</span>
+                </p>
+            </div>
+            <?php
+        }
+    }
+} else {
+    ?>
+    <p class="price">Rempli moi ce panier tocard</p>
+    <?php
 }
-
+?>
+<?php
 include ('include/footer.php');
