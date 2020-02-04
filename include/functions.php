2bd45bb147acbf6bf2bb23d4facca4588b523011
marvin.ferraro <marvin.ferraro@le-campus-numerique.fr>
2020-02-04 16:51:03 +0100

    Modification du panier / Fix Bug

--- include/functions.php
+++ include/functions.php
@@ -27,24 +27,29 @@ function listCats()
 {
 
     $cats = [
-        ["Chiang Mai", "img/chiang_mai.jpg", 1500],
-        ["Bangkok", "img/bangkok.jpg", 1200],
-        ["Lampang", "img/lampang.jpg", 1800]
+        ["Chiang Mai", "img/chiang_mai.jpg", 1500,],
+        ["Bangkok", "img/bangkok.jpg", 1200,],
+        ["Lampang", "img/lampang.jpg", 1800,]
     ];
 
     return $cats;
 }
 
-function afficheArticle($name, $img, $price, $value){
+function afficheArticlePanier($name, $img, $price, $value){
     ?>
     <div class="cadre article">
         <h2 class="nom">Adresse <?= $name ?></h2>
         <img src='<?= $img ?>' alt="Photo de '<?= $name ?> ">
         <p class="price">Pour seulement : <?= $price ?> € <span class="price_text">(Transport compris)</span>
         </p>
+        <p class="info">Cocher pour supprimer un article</p>
         <input class="checkBox" type="checkbox" name="removeArticle[]" value="<?= $value ?>" id="">
     </div>
 <?php
 }
 
 
+function totalPanier($priceT, $somme){
+    $somme += $priceT;
+    return $somme;
+}
--- panier.php
+++ panier.php
@@ -1,33 +1,32 @@
 <?php
 require 'include/functions.php';
-include ('include/head.php');
+include('include/head.php');
 session_start();
 $cats = listCats();
-
 //Si le post n'est pas vide.
-if (!empty($_POST['article'])){
+if (!empty($_POST['article'])) {
 //    il stock les valeurs d'article dans la session Panier
     $_SESSION['panier'] = $_POST['article'];
 }
 
-//var_dump($_POST['removeArticle']);
-//var_dump($_SESSION['panier']);
-//die;
+
 //Si le panier en session n'est pas vide et si la modif
 if (!empty($_SESSION['panier']) AND empty($_POST['removeArticle'])) {
-        $somme = 0;
+    $somme = 0;
     ?>
     <form action="panier.php" method="post">
         <?php
         foreach ($_SESSION['panier'] as $article) {
-            afficheArticle($cats[$article][0],$cats[$article][1],$cats[$article][2],$article);
-            $somme += $cats[$article][2];
+            afficheArticlePanier($cats[$article][0], $cats[$article][1], $cats[$article][2], $article);
+
+            $somme = totalPanier($cats[$article][2], $somme);
         }
         ?>
         <input type="submit" value="Modifier panier">
     </form>
     <p class="priceSomme"> La somme est de <?= $somme ?></p>
     <?php
+
 
     //Sinon si le panier en session est pas vide et le la valeur de la modif non plus
 } elseif (!empty($_POST['removeArticle']) AND !empty($_SESSION['panier'])) {
@@ -42,8 +41,8 @@ if (!empty($_SESSION['panier']) AND empty($_POST['removeArticle'])) {
                 if (!in_array($value, $removeCats)) {
                     array_push($catsNew, $value);
 
-                    afficheArticle($cats[$value][0], $cats[$value][1], $cats[$value][2], $value);
-                    $somme += $cats[$value][2];
+                    afficheArticlePanier($cats[$value][0], $cats[$value][1], $cats[$value][2], $value);
+                    $somme = totalPanier($cats[$value][2], $somme);
                 }
             }
             ?>
@@ -52,19 +51,19 @@ if (!empty($_SESSION['panier']) AND empty($_POST['removeArticle'])) {
         <p class="priceSomme"> La somme est de <?= $somme ?></p>
         <?php
     }
+
     $_SESSION['panier'] = $catsNew;
-
-
 }
 
-if(empty($_SESSION['panier']) AND isset($_POST['removeArticle'])){
+if (empty($_SESSION['panier'])) {
+
         ?>
-    <div class="emptyPanier">
-        <a class="returnCat" href="catalogueV3.php">Catalogue</a>
-        <p class="price">Rempli moi ce panier</p>
-    </div>
+        <div class="emptyPanier">
+            <p><a class="returnCat" href="catalogueV3.php">Catalogue</a></p>
+            <p class="price">Remplis moi ce panier</p>
+        </div>
         <?php
 }
 ?>
 <?php
-include ('include/footer.php');
+include('include/footer.php');
